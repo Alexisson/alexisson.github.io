398c80b85655005e8da38ca554f8387b17f60b12
Ivan Shirokov <ivan.a.shirokov@ya.ru>
2021-03-12 10:22:58 +0600

    io

---
+++ 2021_02_22_1/chain/index.js
@@ -0,0 +1,8 @@
+const { o3 } = require("goss_proto");
+let output = [
+  o3.name,
+  o3.prototype.name,
+  o3.prototype.prototype.name,
+  o3.prototype.prototype.prototype.name,
+];
+console.log(output);
